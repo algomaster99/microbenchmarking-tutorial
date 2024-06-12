26d616f293195d599d3e0c80f0b872c00a6fc923
Aman Sharma <mannu.poski10@gmail.com>
2024-06-12 20:22:32 +0200

    Challenge with JMH

---
+++ src/main/java/io/github/algomaster99/_11Sum_C1C2_Challenge.java
@@ -0,0 +1,51 @@
+package io.github.algomaster99;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.io.IOException;
+
+public class _11Sum_C1C2_Challenge {
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(new String[]{"_11Sum_C1C2_Challenge"});
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value = 1)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 1)
+    public void sum() {
+        int[] arr = new int[1000000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        sum(arr);
+    }
+
+    private static int sum(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    //    Benchmark                            Mode    Cnt     Score    Error  Units
+    //    _11Sum_C1C2_Challenge.sum           thrpt         1472.511           ops/s
+    //    _11Sum_C1C2_Challenge.sum            avgt            0.001            s/op
+    //    _11Sum_C1C2_Challenge.sum          sample  14934     0.001 ±  0.001   s/op
+    //    _11Sum_C1C2_Challenge.sum:p0.00    sample            0.001            s/op
+    //    _11Sum_C1C2_Challenge.sum:p0.50    sample            0.001            s/op
+    //    _11Sum_C1C2_Challenge.sum:p0.90    sample            0.001            s/op
+    //    _11Sum_C1C2_Challenge.sum:p0.95    sample            0.001            s/op
+    //    _11Sum_C1C2_Challenge.sum:p0.99    sample            0.001            s/op
+    //    _11Sum_C1C2_Challenge.sum:p0.999   sample            0.002            s/op
+    //    _11Sum_C1C2_Challenge.sum:p0.9999  sample            0.003            s/op
+    //    _11Sum_C1C2_Challenge.sum:p1.00    sample            0.003            s/op
+    //    _11Sum_C1C2_Challenge.sum              ss            0.005            s/op
+}
