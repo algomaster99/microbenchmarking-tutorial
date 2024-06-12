67ddbc0412e035125a9a3baeea335bb9a7d265b9
Aman Sharma <mannu.poski10@gmail.com>
2024-06-12 20:20:05 +0200

    Add running with warmup stats

---
+++ src/main/java/io/github/algomaster99/_05MillionsOfDummy_OnlyInterpreter_Warmup.java
@@ -0,0 +1,16 @@
+package io.github.algomaster99;
+
+public class _05MillionsOfDummy_OnlyInterpreter_Warmup {
+    static {
+        long start = System.currentTimeMillis();
+        Dummy.createNDummies(1000000);
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken by static method: " + (end - start) + "ms");
+    }
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Dummy.createNDummies(1000000);
+        long end = System.currentTimeMillis();
+        System.out.println("Time taken: " + (end - start) + "ms");
+    }
+}
