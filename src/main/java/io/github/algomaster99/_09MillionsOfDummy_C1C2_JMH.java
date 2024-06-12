16cc0423c66300feae452daa2d10877583c39714
Aman Sharma <mannu.poski10@gmail.com>
2024-06-12 20:20:29 +0200

    Add JMH samples

---
+++ src/main/java/io/github/algomaster99/_09MillionsOfDummy_C1C2_JMH.java
@@ -0,0 +1,25 @@
+package io.github.algomaster99;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.io.IOException;
+
+public class _09MillionsOfDummy_C1C2_JMH {
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(new String[]{"_09MillionsOfDummy_C1C2_JMH"});
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value = 1)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 1)
+    public void createNDummies() {
+        Dummy.createNDummies(1000000);
+    }
+}
